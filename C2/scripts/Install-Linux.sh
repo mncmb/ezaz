85aeefd29f1c59014ad06bc4a6f1d6148eb07f75
mncmb <mncmb@users.noreply.github.com>
2024-06-24 22:16:48 +0200

    Update Install-Linux.sh
    added kasm install

--- C2/scripts/Install-Linux.sh
+++ C2/scripts/Install-Linux.sh
@@ -10,7 +10,7 @@ touch /etc/DOAZLAB/DOAZLAB.Log
 cd /tmp
 curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.15.0.06fdc8.tar.gz
 tar -xf kasm_release_1.15.0.06fdc8.tar.gz
-sudo bash kasm_release/install.sh --admin-password 
+echo y | sudo bash kasm_release/install.sh --admin-password 'Random.Password1515'
 
 ## needs a desktop env
 # apt update
